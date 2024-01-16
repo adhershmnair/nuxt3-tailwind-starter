cd09daed7f7de82099f3ef11f24531be99791793
Adharsh M <adharsh.m@vu.edu.au>
2024-01-17 02:54:53 +0530

    Added daisyui and eslint configuration.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:vue/vue3-recommended',
+      'standard',
+      'prettier',
+      "plugin:prettier/recommended"
+    ],
+    plugins: ['vue'],
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+    }
+  }
